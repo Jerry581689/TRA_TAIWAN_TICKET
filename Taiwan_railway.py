ca23e1e9c2d796e2b7e8507c644e5bf764d650b6
Jerry581689 <Jerry581689@gmail.com>
2024-03-12 22:14:39 +0800

    新增家用電腦設定

--- Taiwan_railway.py
+++ Taiwan_railway.py
@@ -9,20 +9,25 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 
 
-
-chrome_path = "C:\\Users\\User\\OneDrive\\桌面\\chrome-win64\\chrome.exe"   # 指定 Chrome 浏览器的路径
+chrome_path = "C:\\Users\\User\\Desktop\\chrome-win64\\chrome-win64\\chrome.exe" #HOME
+#chrome_path = "C:\\Users\\User\\OneDrive\\桌面\\chrome-win64\\chrome.exe"   # 指定 Chrome 浏览器的路径 公司
 chrome_options = webdriver.ChromeOptions()  # 创建 ChromeOptions 对象，设置浏览器路径
 chrome_options.binary_location = chrome_path
 driver = webdriver.Chrome(options=chrome_options)# 创建 Chrome WebDriver 对象，并指定 Chrome 浏览器和 Chrome WebDriver 的路径
-driver.maximize_window() # 將視窗最大化
+screen_width = driver.execute_script("return window.screen.width;")
+screen_height = driver.execute_script("return window.screen.height;")
+# 將視窗調整為左半邊
+driver.set_window_position(0, 0)
+driver.set_window_size(screen_width // 2, screen_height)
+# driver.maximize_window() # 將視窗最大化
 driver.get("https://www.railway.gov.tw/tra-tip-web/tip/tip001/tip123/query")
 
 
 # 填入參數
-ID           = "A139325062"
+ID           = "L125512387"
 STARTSTATION = "松山"
 ENDSTATION   = "豐原"
-DATE         = "20240404"
+DATE         = "20240403"
 TIME1        = "18:30"
 TIME2        = "23:59"
 
@@ -72,7 +77,7 @@ except Exception:
       print("找不到元素，有車票喔喔喔")
 
 #while True:
-for i in range(1):
+for i in range(3):
     if text == "系統依您所設定的訂票內容查詢，目前查無可售座位，請您調整訂票內容後再重新查詢！":    
         reset_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='reset']")))
         driver.execute_script("arguments[0].click();", reset_button)
@@ -86,12 +91,16 @@ for i in range(1):
         h2_element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'h2.icon-fa.warning')))
         text = h2_element.text
     else:
-
-        link_element = driver.find_element(By.XPATH, '//a[contains(text(), "自強(3000)")]')
-        print(link_element)
-        tr_element = link_element.find_element(By.XPATH, './ancestor::tr[@class="trip-column"]')
-        label_element = tr_element.find_element(By.CSS_SELECTOR, 'label[for]')
-        label_element.click()    
+        time.sleep(1)
+        try:
+            link_element = driver.find_element(By.XPATH, '//a[contains(text(), "自強(3000)")]')     
+            tr_element = link_element.find_element(By.XPATH, './ancestor::tr[@class="trip-column"]')
+            label_element = tr_element.find_element(By.CSS_SELECTOR, 'label[for]')
+            label_element.click()    
+        except Exception:
+            print("沒有自強3000 請自己選擇!!")
+        
+        
 
         input("請手動處理 reCAPTCHA，按下回車繼續...")
 
